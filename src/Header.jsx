7e2a833d4570556f506169d5fd90becc006cbafa
Annay Paul <annay.paul@g.bracu.ac.bd>
2024-01-31 20:42:21 +0600

    [Styling]: Testing out a basic dark-light toggle

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+
 function Header() {
+    const [isFakeDark, setIsFakeDark] = useState(false);
+
+    useEffect(
+        function () {
+            document.documentElement.classList.toggle("fake-dark-mode");
+        },
+        [isFakeDark]
+    );
     return (
         <div className="navbar">
             <h3 className="active">Home</h3>
             <h3>Home</h3>
-            <h3>Home</h3>
+            <button
+                onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+                className="btn-fake-dark-mode"
+            >
+                {isFakeDark ? "☀️" : "🌙"}
+            </button>
         </div>
     );
 }
