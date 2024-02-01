ccde8cfd620f1f690d25809913aaf0b3811cac7a
Annay Paul <annay.paul@g.bracu.ac.bd>
2024-02-01 14:09:26 +0600

    [Fix]: Fixed the default image import for Station.jsx

--- src/components/Station.jsx
+++ src/components/Station.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import radio from "./radio.png";
+import radio from "../radio.png";
 import { useStation } from "../contexts/StationContext";
 
 function Station({ station }) {
